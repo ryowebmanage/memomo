0bd5c4e3e4f5ec582cae42db99b85779c1d7b95b
abson <78572887+ryowebmanage@users.noreply.github.com>
2021-09-01 01:55:59 +0200

    Extract components

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { string } from 'prop-types';
+import { string, bool } from 'prop-types';
 
 function Hello(props) {
-  const { children } = props;
+  const { children, bang } = props;
   return(
       <View>
-       <Text style={styles.text}>
-         {`Hello ${children}`}
-       </Text>
+
       </View>
   );
 }
 
 Hello.protoTypes = {
   children: string.isRequired,
+  bang: bool,
 };
+
+Hello.defaultProps = {
+  bang: false,
+}
 
 const styles = StyleSheet.create({
   text: {
